8bb5968236970c54ee2e6d158987c5fe6dcb1567
diogoruivo47 <20240584@novaims.unl.pt>
2025-05-21 14:35:47 +0100

    Add files via upload

---
+++ exercises/config.py
@@ -0,0 +1,9 @@
+# config.py
+
+API_KEY = 'insert api key here'
+API_SECRET = 'insert your secret key here'
+bot_token = 'telegram bot token'
+channel_id = 'telegram channel id'
+SYMBOL = 'BTCUSDT'
+INTERVAL = '15'
+SESSION_START_HOUR = 0  # UTC
